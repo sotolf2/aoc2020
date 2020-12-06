921b9620b6f655c0209ae9edc9b2bbceedd99af7
Sotolf2 <trym.karlsen@protonmail.ch>
2020-12-06 10:08:16 +0100

    and part2

--- day6.fsx
+++ day6.fsx
@@ -8,20 +8,29 @@ let rec parseGroup lns acc =
     match lns with
     | [] -> acc
     | (hd::tl) ->
-        parseGroup tl (Set.union (Set.ofSeq hd) acc)
+        parseGroup tl ((Set.ofSeq hd)::acc)
 
 let rec parseGroups lns cur acc =
     match lns with
     | [] when List.isEmpty cur -> acc
-    | [] -> parseGroups [] [] ((parseGroup cur Set.empty)::acc)
-    | (hd::tl) when hd = "" -> parseGroups tl [] ((parseGroup cur Set.empty)::acc)
+    | [] -> parseGroups [] [] ((parseGroup cur [])::acc)
+    | (hd::tl) when hd = "" -> parseGroups tl [] ((parseGroup cur [])::acc)
     | (hd::tl) -> parseGroups tl (hd::cur) acc
 
 let parse lns = 
     parseGroups lns [] []
 
 let part1 frms =
-    List.sumBy Set.count frms
+    List.map (List.fold Set.union Set.empty) frms
+    |> List.sumBy Set.count 
     |> printfn "Part1: %d"
 
+let part2 frms =
+    List.map (Set.intersectMany) frms
+    |> List.sumBy Set.count
+    |> printfn "Part2: %d"
+
 let forms = getData filename |> parse
+
+part1 forms
+part2 forms
