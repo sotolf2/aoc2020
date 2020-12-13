ada56ddff78563f9c504b36db08a1e40b44cbf53
Sotolf2 <trym.karlsen@protonmail.ch>
2020-12-13 16:27:43 +0100

    d13p2

--- day13.fsx
+++ day13.fsx
@@ -29,6 +29,28 @@ let part1 (arrival, busses) =
     |> (fun (bId, time) -> (time - arrival) * bId)
     |> printfn "Part1: %d"
 
+let parse2 (lines: list<string>) =
+    lines.[1].Split(',')
+    |> Array.map (fun x -> if x = "x" then None else Some (uint64 x))
+    |> List.ofArray
+    |> List.mapi (fun i x -> (((uint64 i)), x))
+    |> List.filter (fun (_,x) -> x <> None)
+    |> List.map (fun (i, x) -> (i, Option.get x))
+
+let rec searchLine (busses:list<uint64*uint64>) cur inc  =
+    match busses with
+    | [] -> cur
+    | (hd::tl) ->
+        if (cur + (fst hd)) % (snd hd) = 0UL then 
+            //printfn "found place for hd: %A with cur: %d and inc: %d" hd cur inc |> ignore
+            searchLine tl cur (inc * snd hd) else
+        searchLine busses (cur + inc) inc 
+
+let part2 busses =
+    searchLine busses 0UL 1UL
+    |> printfn "Part2: %d"
+
 let timetable = getData filename |> parse
 part1 timetable
-
+let busses = getData filename |> parse2
+part2 busses
