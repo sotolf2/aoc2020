0f0c8587e03c850794fbb5bfa7450db1596a9edc
Sotolf2 <trym.karlsen@protonmail.ch>
2020-12-13 09:54:59 +0100

    d13p1

---
+++ day13.fsx
@@ -0,0 +1,33 @@
+let filename = "day13.txt"
+
+let getData filename = 
+    System.IO.File.ReadAllLines filename 
+    |> List.ofSeq
+
+let parse (lines: list<string>) =
+    let arrival = uint32 (List.head lines)
+    let busses = lines.[1].Split(',') 
+                |> Array.filter (fun x -> not <| (x = "x")) 
+                |> Array.map uint32 
+                |> List.ofArray
+
+    (arrival, busses)
+
+let genInfinite from = 
+    Seq.initInfinite (fun i -> ((uint32 i) + 1u) * from)
+
+let part1 (arrival, busses) =
+    let closestTimes =
+        busses
+        |> List.map (fun x -> 
+                        genInfinite x 
+                        |> Seq.skipWhile (fun x -> x < arrival)
+                        |> Seq.head)
+    
+    List.zip busses closestTimes
+    |> List.minBy snd
+    |> (fun (bId, time) -> (time - arrival) * bId)
+    |> printfn "Part1: %d"
+
+let data = getData filename |> parse
+part1 data
